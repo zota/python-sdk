ec8ca33670c1a0e075879cf16445c81fa96cc0e2
alexbhr <alexbhr@users.noreply.github.com>
2026-06-22 15:23:51 +0000

    Update test API key retrieval to force the use of secrets

--- zotasdk/tests/conftest.py
+++ zotasdk/tests/conftest.py
@@ -1,9 +1,10 @@
+import os
 import pytest
 
 from zotasdk.client import MGClient
 
 TEST_MERCHANT_ID = "SDKEXPLORER"
-TEST_MERCHANT_SECRET_KEY = "7801a7a3-0e81-481e-8f59-ee0bfef009bb"
+TEST_MERCHANT_SECRET_KEY = os.getenv('API_MERCHANT_SECRET_KEY', 'test-secret-key-placeholder')
 MOCK_ENDPOINT_URL = "http://localhost:5000"
 
 
